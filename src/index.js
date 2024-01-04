1397b39392fe48052237555c106454e8bd2b04c5
Adrienne Ortiz <ani650@gmail.com>
2024-01-05 06:13:29 +0800

    Initial Foundation Codes

---
+++ src/index.js
@@ -0,0 +1,72 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
+import { Notify } from 'notiflix';
+
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+
+const selectBreed = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+const loadEl = document.querySelector('.load');
+const errorEl = document.querySelector('.error');
+
+function breedSelection(data) {
+  fetchBreeds(data)
+    .then(data => {
+      loadEl.classList.replace('loader', 'is-hidden');
+
+      const optionsMarkup = data
+        .map(({ name, id }) => `<option value='${id}'>${name}</option>`)
+        .join('');
+
+      selectBreed.insertAdjacentHTML('beforeend', optionsMarkup);
+
+      new SlimSelect({
+        select: selectBreed,
+      });
+
+      selectBreed.classList.remove('is-hidden');
+    })
+    .catch(onError);
+}
+
+breedSelection();
+
+function createMarkup(event) {
+  loadEl.classList.replace('is-hidden', 'loader');
+  selectBreed.classList.add('is-hidden');
+  catInfo.classList.add('is-hidden');
+
+  const breedId = event.target.value;
+
+  fetchCatByBreed(breedId)
+    .then(data => {
+      loadEl.classList.replace('loader', 'is-hidden');
+      selectBreed.classList.remove('is-hidden');
+
+      const { url, breeds } = data[0];
+      const { name, description, temperament } = breeds[0];
+
+      catInfo.innerHTML = `
+        <img src="${url}" alt="${name}" width="400"/>
+        <div class="box">
+          <h2>${name}</h2>
+          <p>${description}</p>
+          <p><strong>Temperament:</strong> ${temperament}</p>
+        </div>
+      `;
+      catInfo.classList.remove('is-hidden');
+    })
+    .catch(onError);
+}
+
+selectBreed.addEventListener('change', createMarkup);
+
+function onError() {
+  selectBreed.classList.remove('is-hidden');
+  errorEl.classList.replace('loader', 'is-hidden');
+
+  Notify.failure(
+    'Oops! Something went wrong! Try reloading the page or select another cat breed!'
+  );
+}
