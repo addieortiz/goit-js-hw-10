686531da48bb8c1d4259ea9bff52070932cfaf15
Adrienne Ortiz <ani650@gmail.com>
2024-01-05 09:01:29 +0800

    Update codes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import { Notify } from 'notiflix';
-// import SlimSelect from 'slim-select';
-// import 'slim-select/dist/slimselect.css';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
 const selectBreed = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
-const loadEl = document.querySelector('.load');
-const errorEl = document.querySelector('.error');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
 function breedSelection(data) {
   fetchBreeds(data)
     .then(data => {
-      loadEl.classList.replace('loader', 'is-hidden');
+      loader.classList.replace('loader', 'is-hidden');
 
       let markupOptions = data.map(({ name, id }) => {
-        return `<option value='${id}'>${name}</option>`;
+        return `<option value ='${id}'>${name}</option>`;
       });
 
       selectBreed.insertAdjacentHTML('beforeend', markupOptions);
 
-      //   new SlimSelect({
-      //     select: selectBreed,
-      //   });
+      new SlimSelect({
+        select: selectBreed,
+      });
 
       selectBreed.classList.remove('is-hidden');
     })
@@ -31,7 +31,7 @@ function breedSelection(data) {
 breedSelection();
 
 function createMarkup(event) {
-  loadEl.classList.replace('is-hidden', 'loader');
+  loader.classList.replace('is-hidden', 'loader');
   selectBreed.classList.add('is-hidden');
   catInfo.classList.add('is-hidden');
 
@@ -39,19 +39,19 @@ function createMarkup(event) {
 
   fetchCatByBreed(breedId)
     .then(data => {
-      loadEl.classList.replace('loader', 'is-hidden');
+      loader.classList.replace('loader', 'is-hidden');
       selectBreed.classList.remove('is-hidden');
 
       const { url, breeds } = data[0];
       const { name, description, temperament } = breeds[0];
 
       catInfo.innerHTML = `
-        <img src="${url}" alt="${name}" width="400"/>
-        <div class="box">
-          <h2>${name}</h2>
-          <p>${description}</p>
-          <p><strong>Temperament:</strong> ${temperament}</p>
-        </div>
+      <img src="${url}" alt="${name}" width="400"/>
+      <div class="box">
+        <h2>${name}</h2>
+        <p>${description}</p>
+        <p><strong>Temperament:</strong> ${temperament}</p>
+      </div>
       `;
       catInfo.classList.remove('is-hidden');
     })
@@ -61,9 +61,8 @@ function createMarkup(event) {
 selectBreed.addEventListener('change', createMarkup);
 
 function onError() {
-  //   selectBreed.classList.remove('is-hidden');
-  //   errorEl.classList.replace('loader', 'is-hidden');
-
+  error.classList.remove('is-hidden');
+  selectBreed.classList.add('is-hidden');
   Notify.failure(
     'Oops! Something went wrong! Try reloading the page or select another cat breed!'
   );
